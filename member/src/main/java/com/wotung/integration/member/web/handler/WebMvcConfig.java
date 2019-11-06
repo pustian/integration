cad0579a5a1f1242bd928472e9b62a77a21768ab
pustian <pustian@msn.com>
2019-11-06 13:48:18 +0800

    跨域

--- member/src/main/java/com/wotung/integration/member/web/handler/WebMvcConfig.java
+++ member/src/main/java/com/wotung/integration/member/web/handler/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.wotung.integration.member.web.handler;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,15 +19,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     ParameterInterceptor parameterInterceptor;
 
-//    // 跨域问题　https://www.cnblogs.com/yuansc/p/9076604.html
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("*")
-//                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
-//                .maxAge(3600)
-//                .allowCredentials(true);
-//    }
+    // 跨域问题　https://www.cnblogs.com/yuansc/p/9076604.html
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .maxAge(3600)
+                .allowCredentials(true);
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
